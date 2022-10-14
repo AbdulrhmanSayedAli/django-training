792c6374cba2b0167a17700b1f7c80d5482f50bf
AbdulrhmanSayedAli <roctes7@gmail.com>
2022-10-15 01:22:59 +0300

    fixing album admin

--- albums/admin.py
+++ albums/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Album
 # Register your models here.
-admin.site.register(Album)
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    readonly_fields = ('creation_datetime',)
+    list_display = ("name", "is_approved")
