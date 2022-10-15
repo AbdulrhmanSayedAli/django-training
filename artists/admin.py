5f871b4c1262645736d6c9f1355a657bbb4dd89b
AbdulrhmanSayedAli <roctes7@gmail.com>
2022-10-15 10:47:26 +0300

    fixing inline problem

--- artists/admin.py
+++ artists/admin.py
@@ -6,6 +6,9 @@ from albums.models import Album
 class AlbumInline(admin.TabularInline):
     model = Album
 
+    def get_extra(self, request, obj=None, **kwargs):
+        return 0
+
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ("stage_name", "social_link", "approved_albums")
