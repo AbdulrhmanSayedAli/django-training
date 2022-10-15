47575039b3e64fcb0a39417bf55ad5e720f658d8
AbdulrhmanSayedAli <roctes7@gmail.com>
2022-10-15 10:21:33 +0300

    fixing inline problem

--- artists/admin.py
+++ artists/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
 from .models import Artist
+from albums.models import Album
 # Register your models here.
 
-
+class AlbumInline(admin.TabularInline):
+    model = Album
 
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ("stage_name", "social_link", "approved_albums")
 
-    fieldsets = (
-        (None, {
-            'fields': ('stage_name',"social_link","albums")
-        }),
 
-    )
+    inlines = [
+        AlbumInline,
+    ]
 
     
 
